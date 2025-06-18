ae77eea2abfc6291fbfc91472aed5ae01d457688
Djordje Kitic <djordjekitic2003@gmail.com>
2025-06-18 19:52:02 +0200

    Dodata validacija za model Knjiga i kontrolu validacije u BookController

--- RazmenaKnjiga/Models/Knjige.cs
+++ RazmenaKnjiga/Models/Knjige.cs
@@ -1,5 +1,7 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;  // Dodaj ovo
 
 namespace RazmenaKnjiga.Models
 {
@@ -10,17 +12,25 @@ namespace RazmenaKnjiga.Models
         public string Id { get; set; }
 
         [BsonElement("Naslov")]
+        [Required(ErrorMessage = "Naslov je obavezan")]
+        [StringLength(100, ErrorMessage = "Naslov ne sme biti duži od 100 karaktera")]
         public string Naslov { get; set; }
 
         [BsonElement("Autor")]
+        [Required(ErrorMessage = "Autor je obavezan")]
+        [StringLength(100, ErrorMessage = "Autor ne sme biti duži od 100 karaktera")]
         public string Autor { get; set; }
 
+        [StringLength(50)]
         public string Zanr { get; set; }
 
         public string Opis { get; set; }
 
+        [StringLength(50)]
         public string Stanje { get; set; }  // npr. Novo, Polovno, Oštećeno
 
+        [Required(ErrorMessage = "Grad je obavezan")]
+        [StringLength(50)]
         public string Grad { get; set; }
 
         public string SlikaBase64 { get; set; }  // Slika kao Base64 string
--- RazmenaKnjiga/Program.cs
+++ RazmenaKnjiga/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<UserService>();
+builder.Services.AddSingleton<BookService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
